cd47c84949a231522429b636fa05f4f80f5911ec
johe07003 <johe07003@E-PW05C37X>
2024-10-10 12:16:04 +0200

    Add project files.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace hej
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("vilken bok gillar?");
+            string book=Console.ReadLine();
+            Console.WriteLine("boken " + book + " är jätte bra");
+        }
+    }
+}
